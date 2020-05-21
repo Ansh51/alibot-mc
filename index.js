22275e373a371d0da4f905b41f90143c0971add4
uAliFurkanY <alifurkany@gmail.com>
2020-05-21 14:41:42 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,186 @@
+let config = require(require("path").join(__dirname, "config.json"));
+const isVarSet = () => !!(config.HOST && config.USERNAME && config.PASSWORD && config.OP && config.MODE && config.ACTIVE && config.WEBSITE);
+if (!isVarSet()) {
+	console.log("Run setup.js and try again.");
+	process.exit(0);
+}
+if (config.ACTIVE === "false") {
+	process.exit(0);
+}
+
+const mineflayer = require("mineflayer");
+
+function send(msg = "/help") {
+	toSend.push(msg);
+}
+
+function msg(msg, u) {
+	send("/msg " + u + " " + msg);
+}
+
+function randStr(length) {
+	let result = "";
+	let characters =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	let charactersLength = characters.length;
+	for (let i = 0; i < length; i++) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
+	return result;
+}
+
+let op = config.OP.split(",");
+console.log("Operators: " + op);
+
+let lastkill = Date.now();
+let start = Date.now();
+let username;
+
+let toSend = [];
+setInterval(() => {
+	if (toSend.length !== 0 && Date.now() >= start + 15 * 1000) {
+		bot.chat(toSend[0]);
+		toSend.shift();
+	}
+}, 1500);
+
+let session = false;
+
+let login = {
+	host: config.HOST,
+	username: config.USERNAME,
+	password: config.PASSWORD,
+	session: session,
+};
+
+let version = "0.0.1";
+let mode = config.MODE;
+let firstchat = true;
+
+let bot;
+
+function init(r) {
+	console.log(`[${Date.now()}] Init ${r}`);
+	bot = mineflayer.createBot(login);
+
+	toSend = [];
+
+	lastkill = Date.now();
+	start = Date.now();
+
+	function main() {
+		console.log("Spawned.");
+		// send("/msg " + op[0] + " Logged in.");
+		// bot.on("", (u, m, t, rm) => {});
+		username = bot.player.username;
+		bot.chatAddPattern(
+			/^[a-zA-Z_]{3,16} wants to teleport to you\.$/,
+			"tpa",
+			"received tpa"
+		);
+		bot.chatAddPattern(
+			/^[a-zA-Z_]{3,16} whispers: /,
+			"msg",
+			"received msg"
+		);
+		bot.on("tpa", (u, m, t, rm) => {
+			let user = m.extra[0].text;
+			if (op.includes(user) || mode !== "private") {
+				send("/msg " + user + " Accepting...");
+				send("/tpy " + user);
+			} else {
+				send(
+					"/msg " +
+					user +
+					" Declining! You are not in the operators list and the mode is private."
+				);
+				send("/tpn " + user);
+			}
+		});
+		bot.on("msg", (u, m, t, rm) => {
+			m = m.extra[0].text.trim();
+			u = m.split(" ")[0];
+			m = m.split(": ")[1];
+			console.log(`${u} -> ${m}`);
+			let args = m.split(" ");
+			args.shift();
+			if (m.startsWith("help")) {
+				msg(config.WEBSITE || "https://github.com/uAliFurkanY/alibot-mc/", u);
+			} else if (m.startsWith("kill")) {
+				op.includes(u) ||
+					(Date.now() >= lastkill + 15 * 1000 && mode !== "private")
+					? send("/kill")
+					: msg(
+						`Declining! You're not an operator and the mode is ${mode}.`,
+						u
+					);
+			} else if (m.startsWith("op")) {
+				if (op.includes(u)) {
+					op.push(args[0]);
+					msg(`Opped ${args[0]}`, u);
+				} else {
+					msg("You can't OP people.", u);
+				}
+			} else if (m.startsWith("listop")) {
+				msg(op.join(","), u);
+			} else if (m.startsWith("discord")) {
+				msg("Under construction.", u);
+			} else if (m.startsWith("ping")) {
+				if (args.length >= 1) {
+					msg(`${args[0]}'s ping is ${bot.players[args[0]].ping}ms.`, u);
+				} else {
+					msg(`Your ping is ${bot.players[u].ping}ms.`, u);
+				}
+			} else if (m.startsWith("mode")) {
+				op.includes(u) && args.length >= 1
+					? msg(
+						`Changing the mode to ${args[0]}.`,
+						u
+					) && (mode = args[0])
+					: msg(`The mode is ${mode}`, u);
+			} else if (m.startsWith("reinit")) {
+				op.includes(u) ? init("reinit") : msg("You are not an operator.", u);
+			} else if (m.startsWith("random")) {
+				if (args.length === 0) {
+					msg(`Usage: random [dice|number <min> <max>]`, u);
+				} else if (args[0] === "number") {
+					if (args.length >= 4) {
+						if (parseInt(args[1]) !== NaN && parseInt(args[2]) !== NaN) {
+							let nums = [parseInt(args[1]), parseInt(args[2])];
+							if (nums[1] > nums[0]) {
+								msg(`Your random number is ${Math.floor(Math.random() * (nums[1] - nums[0] + 1)) + 1}.`, u);
+							} else {
+								msg(`Minimum is larger than maximum.`, u);
+							}
+						} else {
+							msg(`You did not provide a number.`, u);
+						}
+					} else {
+						msg(`Usage: random [dice|number <min> <max>]`, u);
+					}
+				} else if (args[0] === "dice") {
+					msg(`You rolled ${Math.floor(Math.random() * (6 - 1 + 1)) + 1}.`, u);
+				}
+			}
+		});
+	}
+	bot.once("spawn", main);
+	bot._client.once("session", () => {
+		session = bot._client.session;
+		login.session = session;
+	});
+	bot.once("login", () => console.log("Logged in."));
+	bot.once("kick", () => init("Kick"));
+	bot.once("end", () => setTimeout(() => init("End"), 10 * 1000));
+	bot.once("error", (m) => {
+		if (m.message === "Invalid session.") {
+			session = false;
+			init("Error " + m);
+		} else if (
+			m.message === "Invalid credentials. Invalid username or password."
+		) {
+			setTimeout(() => init("Error"), 10 * 60 * 1000);
+		}
+	});
+}
+init("First Start");
---
+++ setup.js
@@ -0,0 +1,36 @@
+const rl = require("readline-sync");
+const fs = require("fs");
+let host, user, pass, op, mode, active, website;
+host = rl.question("Enter the server that the bot will run on: ");
+user = rl.question("Enter the MC account email/username: ");
+pass = rl.question("Enter the MC account password: ", {
+	hideEchoBack: true
+});
+op = rl.question("Enter the operators of the bot separated by ',': ");
+let modes = ["public", "private"];
+mode = modes[rl.keyInSelect(modes, "Select the mode: ")] || "public";
+website = rl.question("Enter the help link: ");
+active = rl.keyInYNStrict("Should it be active? ") ? "true" : "false";
+console.clear();
+console.log("Server: " + host);
+console.log("User: " + user);
+console.log("Password: " + pass);
+console.log("Operators: " + op);
+console.log("Mode: " + mode);
+console.log("Active: " + active);
+console.log("Website: " + website);
+if(rl.keyInYNStrict("Is this ok? ")) {
+	console.clear();
+	fs.writeFileSync("config.json", JSON.stringify({
+		HOST: host,
+		USERNAME: user,
+		PASSWORD: pass,
+		OP: op,
+		MODE: mode,
+		ACTIVE: active,
+		WEBSITE: website
+	}));
+	rl.keyInPause("File created. You now can run the program.");
+} else {
+	rl.keyInPause("OK. Cancelling...");
+}
