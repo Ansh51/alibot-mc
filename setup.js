a87634aefd5f84d9a4fa57831cfe35402b3822e5
uAliFurkanY <alifurkany@gmail.com>
2020-05-24 18:33:57 +0300

    remove website option from setup

--- setup.js
+++ setup.js
@@ -1,6 +1,6 @@
 const rl = require("readline-sync");
 const fs = require("fs");
-let host, user, pass, op, mode, active, website;
+let host, user, pass, op, mode, active;
 host = rl.question("Enter the server that the bot will run on: ");
 user = rl.question("Enter the MC account email/username: ");
 pass = rl.question("Enter the MC account password: ", {
@@ -9,7 +9,6 @@ pass = rl.question("Enter the MC account password: ", {
 op = rl.question("Enter the operators of the bot separated by ',': ");
 let modes = ["public", "private"];
 mode = modes[rl.keyInSelect(modes, "Select the mode: ")] || "public";
-website = rl.question("Enter the help link: ");
 active = rl.keyInYNStrict("Should it be active? ") ? "true" : "false";
 console.clear();
 console.log("Server: " + host);
@@ -18,7 +17,6 @@ console.log("Password: " + pass);
 console.log("Operators: " + op);
 console.log("Mode: " + mode);
 console.log("Active: " + active);
-console.log("Website: " + website);
 if(rl.keyInYNStrict("Is this ok? ")) {
 	console.clear();
 	fs.writeFileSync("config.json", JSON.stringify({
@@ -27,8 +25,7 @@ if(rl.keyInYNStrict("Is this ok? ")) {
 		PASSWORD: pass,
 		OP: op,
 		MODE: mode,
-		ACTIVE: active,
-		WEBSITE: website
+		ACTIVE: active
 	}));
 	rl.keyInPause("File created. You now can run the program.");
 } else {
